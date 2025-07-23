6e908056941b354cc7e32bd2b6e3b2e0646b2485
Matthew Benincasa <flubula@gmail.com>
2025-07-23 01:19:43 -0400

    Create backupScript.sh
    First Commit

---
+++ backupScript.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+virtualHosts=( $(ls -d /var/www/*.domain.com | xargs -n 1 basename))
+for i in "${virtualHosts[@]}"
+do
+timeStamp=$(date +%Y-%m-%d-%H-%M)
+zip -r /var/www/backups/$i-$timeStamp.zip /var/www/$i
+done
